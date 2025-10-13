04f8826c569b533f5d16e0e2dc85a4bdef931606
liat <liat@liatbenshai.com>
2025-10-13 11:02:42 +0300

    force: trigger Vercel rebuild

--- app/documents/advance-directives/page.tsx
+++ app/documents/advance-directives/page.tsx
@@ -5,3 +5,4 @@ import AdvanceDirectivesDocument from '@/components/AdvanceDirectivesDocument';
 export default function AdvanceDirectivesPage() {
   return <AdvanceDirectivesDocument />;
 }
+"// Force rebuild" 
