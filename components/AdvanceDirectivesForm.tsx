520c319380164c374853d672d6b75db916c05387
liatbenshai <48071856+liatbenshai@users.noreply.github.com>
2025-10-16 17:55:45 +0300

    commit

--- components/AdvanceDirectivesForm.tsx
+++ components/AdvanceDirectivesForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FileText, User, Users, Download, Plus, Trash2 } from 'lucide-react';
 import GenderSelector from './GenderSelector';
 import AdvanceDirectivesSectionSelector from './AdvanceDirectivesSectionSelector';
